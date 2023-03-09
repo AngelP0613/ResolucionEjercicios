33b86390ab410bb670c9cf604255c11d517d78be
Angel Peña <110634996+AngelP0613@users.noreply.github.com>
2023-03-09 17:02:11 -0500

    Update and rename Java Básico/Ejercicio Sesión 3/ejercicio3.java to java basico/Ejercicio Sesión 3/ejercicio3.java
    concatenar es realizar la suma de los }strings

--- java basico/Ejercicio Sesión 3/ejercicio3.java
+++ java basico/Ejercicio Sesión 3/ejercicio3.java
@@ -8,8 +8,9 @@ public class ejercicio3 {
         String[] texto = new String[]{"Texto 1", "Texto 2", "Texto 3"};
 
         //Recorremos el array y mostramos su contenido
-        for(int i = 0; i < texto.length; i++) {
-            System.out.print(texto[i]);
+      for (int i = 0; i < nombres.length; i++) {
+            resultado += " " + nombres[i];
+            System.out.println( resultado);
         }
 
     }
